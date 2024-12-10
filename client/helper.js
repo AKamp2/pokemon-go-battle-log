2659cfc7e4823d35b29a242f9a9bb6790b039212
Aidan Kamp <ackamp2@gmail.com>
2024-12-10 18:52:31 -0500

    updated login page layout, replaced domo message

--- client/helper.js
+++ client/helper.js
@@ -3,44 +3,45 @@
    end in an error.
 */
 const handleError = (message) => {
-    document.getElementById('errorMessage').textContent = message;
-    document.getElementById('domoMessage').classList.remove('hidden');
-  };
-  
-  /* Sends post requests to the server using fetch. Will look for various
-     entries in the response JSON object, and will handle them appropriately.
-  */
-  const sendPost = async (url, data, handler) => {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-  
-    const result = await response.json();
-    document.getElementById('domoMessage').classList.add('hidden');
-  
-    if(result.redirect) {
-      window.location = result.redirect;
-    }
-  
-    if(result.error) {
-      handleError(result.error);
-    }
+  const errorPopup = document.getElementById('errorPopup');
+  const errorText = document.getElementById('errorText');
 
-    if(handler) {
-        handler(result);
-    }
-  };
+  errorText.textContent = message;
+  errorPopup.classList.remove('hidden');
+};
 
-  const hideError = () => {
-    document.getElementById('domoMessage').classList.add('hidden');
-  };
+const sendPost = async (url, data, handler) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
 
-  module.exports = {
-    handleError,
-    sendPost,
-    hideError,
-  };
+  const result = await response.json();
+  hideError(); // Ensure any previous errors are hidden
+
+  if (result.redirect) {
+    window.location = result.redirect;
+  }
+
+  if (result.error) {
+    handleError(result.error);
+  }
+
+  if (handler) {
+    handler(result);
+  }
+};
+
+const hideError = () => {
+  const errorPopup = document.getElementById('errorPopup');
+  errorPopup.classList.add('hidden');
+};
+
+module.exports = {
+  handleError,
+  sendPost,
+  hideError,
+};
